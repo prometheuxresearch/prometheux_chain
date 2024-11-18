338db25b6f729f11e403198a4ff1a934659b5d5f
Davide Benedetto <davide.benedetto93@gmail.com>
2024-11-18 18:38:07 +0100

    Updaded documentation

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
 from setuptools import setup, find_packages
+
+# Get the directory where setup.py is located
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+# Read the contents of ReadMe.md
+with open(os.path.join(this_directory, 'ReadMe.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="prometheux_chain",
@@ -8,7 +16,9 @@ setup(
         "requests",
         "pyyaml",
         "pandas",
-        "networkx"
+        "networkx",
+        "pyvis",
+        "ipycytoscape"
     ],
     package_data={
         'prometheux_chain': ['config.yaml'],
@@ -17,12 +27,18 @@ setup(
     author='Prometheux Limited',
     author_email='davben@prometheux.co.uk',
     description='Prometheux chain is a Python SDK designed to build, evolve and deploy your new knowledge graphs.',
-    long_description=open('ReadMe.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/prometheuxresearch/prometheux_chain',
     classifiers=[
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    python_requires='>=3.7',
 )
