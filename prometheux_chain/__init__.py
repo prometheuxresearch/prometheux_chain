590ad8878726efe2b50282525ae22ff427b38b80
Teodoro Baldazzi <teodoro.baldazzi@gmail.com>
2024-12-04 16:50:43 +0100

    GraphRAG v0 SDK side

--- prometheux_chain/__init__.py
+++ prometheux_chain/__init__.py
@@ -14,3 +14,5 @@ from .chat.chat import chat
 from .translator.schema_translator import infer_from_schema
 from .analytics.all_pairs_join import all_pairs_join
 from .reasoning.reasoner import perform
+from .graph_rag.semantic_indexer import semantic_indexing
+from .graph_rag.graph_rag_runner import graph_rag
