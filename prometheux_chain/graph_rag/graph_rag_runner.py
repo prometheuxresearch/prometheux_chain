4b7422e53b82151374e53bbce907a59f2ca8db10
Davide Benedetto <davide.benedetto93@gmail.com>
2024-12-07 02:32:50 +0100

    GraphRAG v0.1

--- prometheux_chain/graph_rag/graph_rag_runner.py
+++ prometheux_chain/graph_rag/graph_rag_runner.py
@@ -2,7 +2,7 @@
 from ..client.JarvisPyClient import JarvisPyClient
 
 
-def graph_rag(question, vada_file_path):
+def graph_rag(question=None, vadalog_program=None):
     """
     Function to process the user's question and output the results.
 
@@ -10,16 +10,19 @@ def graph_rag(question, vada_file_path):
         question (str): The user's natural language question.
         vada_file_path (str): The path to the .vada file.
     """
+    if(not question and not vadalog_program):
+        raise Exception("Please provide a question to ask or a vadalog_program for reasoning or both")
     # Read the vadalog program from the file
-    try:
-        with open(vada_file_path, 'r') as file:
-            vadalog_program = file.read()
-    except FileNotFoundError:
-        print(f"Error: File '{vada_file_path}' not found. Please check the file path.")
-        return None
-    except Exception as e:
-        print(f"Error reading file '{vada_file_path}': {e}")
-        return None
+    if vadalog_program:
+        try:
+            with open(vadalog_program, 'r') as file:
+                vadalog_program = file.read()
+        except FileNotFoundError:
+            print(f"Error: File '{vadalog_program}' not found. Please check the file path.")
+            return None
+        except Exception as e:
+            print(f"Error reading file '{vadalog_program}': {e}")
+            return None
 
     # Initialize the client
     client = JarvisPyClient()
