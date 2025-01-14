79045d6d9deb471b9f6fe403accd38ade36a8301
Teodoro Baldazzi <teodoro.baldazzi@gmail.com>
2025-01-14 17:22:40 +0100

    Set openai as default llm provider

--- prometheux_chain/config.py
+++ prometheux_chain/config.py
@@ -6,6 +6,8 @@ class Config:
     def __init__(self, config_file=None):
         self.config_file = config_file or os.path.join(os.path.dirname(__file__), 'config.yaml')
         self._config = self._load_config_from_file(self.config_file)
+        # Optionally override from environment variables
+        self._override_with_environment()
 
     def _load_config_from_file(self, config_file):
         """Load configuration from a YAML file."""
@@ -64,6 +66,17 @@ class Config:
     def get_all(self):
         """Get the entire configuration as a dictionary."""
         return self._config.copy()  # Return a copy to prevent external modification
+
+    def _override_with_environment(self):
+        """
+        For each key in the loaded config, if there's an environment variable of the SAME NAME,
+        override the config value. For example, if _config has 'PMTX_TOKEN' = 'None'
+        but os.environ['PMTX_TOKEN'] = 'my_token', then we'll override it.
+        """
+        for key in list(self._config.keys()):
+            env_val = os.environ.get(key)
+            if env_val is not None:
+                self._config[key] = env_val
 
     def __getitem__(self, key):
         return self.get(key)
