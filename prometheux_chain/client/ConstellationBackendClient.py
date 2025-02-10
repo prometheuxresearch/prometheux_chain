48faa10ff5d84715e5082e107a7685ea127729cb
Davide Benedetto <davide.benedetto93@gmail.com>
2025-02-10 20:49:11 +0100

    Fixed schema inference API

--- prometheux_chain/client/ConstellationBackendClient.py
+++ prometheux_chain/client/ConstellationBackendClient.py
@@ -6,17 +6,8 @@ from ..model.SchemaInferencePayload import SchemaInferencePayload
 class ConstellationBackendClient:
 
     @staticmethod
-    def infer_from_schema(schema_inference_payload: SchemaInferencePayload):
+    def infer_from_schema(schema_inference_payload : SchemaInferencePayload):
         headers = {'Content-Type': 'application/json'}
         data = json.dumps(schema_inference_payload.to_dict())
-        
-        try:
-            response = requests.post(
-                f"{config['CONSTELLATION_BACKEND_URL']}/schema-info/inferFromSchema",
-                headers=headers,
-                data=data
-            )
-            response.raise_for_status()
-            return response.json()
-        except requests.exceptions.RequestException as e:
-            return {"error": f"Request failed: {e}"}
+        response = requests.post(f"{config['CONSTELLATION_BACKEND_URL']}/schema-info/inferFromSchema", headers=headers, data=data)
+        return response
