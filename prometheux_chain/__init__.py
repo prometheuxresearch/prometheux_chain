f7754e1b184a662e878eec0082e2026be417b2e2
Teodoro Baldazzi <teodoro.baldazzi@gmail.com>
2025-02-10 08:30:14 +0100

    Added back schema translator

--- prometheux_chain/__init__.py
+++ prometheux_chain/__init__.py
@@ -7,5 +7,5 @@ from .explanation.visualizer import visualize_schema
 from .llm.validate import validate
 from .llm.rag import rag
 # from .llm.chat import chat
-# from .translator.schema_translator import infer_from_schema
+from .translator.schema_translator import infer_from_schema
 # from .analytics.all_pairs_join import all_pairs_join
