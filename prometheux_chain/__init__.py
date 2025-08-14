f687aada18dfd27f151bc07cc7cd83806f8763d5
Teodoro Baldazzi <teodoro.baldazzi@gmail.com>
2025-08-14 11:58:18 +0200

    SDK cleanup pre-updates

--- prometheux_chain/__init__.py
+++ prometheux_chain/__init__.py
@@ -1,55 +1,64 @@
 from .config import config
 
-from .project.manage_projects import cleanup_projects
-from .project.manage_projects import save_project
-from .project.manage_projects import list_projects
-from .project.manage_projects import load_project
-
-from .project.manage_data import cleanup_sources
-from .project.manage_data import connect_sources
-from .project.manage_data import list_sources
-
-from .project.manage_notebooks import cleanup_notebooks
-from .project.manage_notebooks import save_notebook
-from .project.manage_notebooks import list_notebooks
-from .project.manage_notebooks import load_notebook
-
-from .project.manage_notebooks import cleanup_cells
-from .project.manage_notebooks import save_cell
-from .project.manage_notebooks import run_cell
-from .project.manage_notebooks import list_cell_outputs
-from .project.manage_notebooks import list_cells
-
-from .project.manage_concepts import list_concepts
-
-from .project.manage_kgs import cleanup_kgs
 
 
 
 
 
 
-from .project.manage_kgs import save_kg
-from .project.manage_kgs import list_kgs
-from .project.manage_kgs import load_kg
-from .project.manage_kgs import save_kg_chat
-from .project.manage_kgs import load_kg_chat
-from .project.manage_kgs import cleanup_kg_chat
-from .project.manage_kgs import save_kg_query
-from .project.manage_kgs import load_kg_queries
-from .project.manage_kgs import cleanup_kg_queries
 
-from .project.compile import compile
-from .project.reason import reason
-from .project.query import query
-from .project.explain import explain
-from .visualization.predicate_graph_visualization import visualize_predicate_graph
-from .visualization.kg_schema_visualization import visualize_kg_schema
-from .llm.validate import validate
-from .llm.rag import rag
-from .llm.translate import translate
-from .llm.chat import chat
-from .translation.translate_from_rdf import translate_from_rdf
-from .project.analytics import infer_schema
-from .project.analytics import all_pairs_join
-from .project.analytics import kg_overview
+
+# from .project.manage_projects import cleanup_projects
+# from .project.manage_projects import save_project
+# from .project.manage_projects import list_projects
+# from .project.manage_projects import load_project
+
+# from .project.manage_data import cleanup_sources
+# from .project.manage_data import connect_sources
+# from .project.manage_data import list_sources
+
+# from .project.manage_notebooks import cleanup_notebooks
+# from .project.manage_notebooks import save_notebook
+# from .project.manage_notebooks import list_notebooks
+# from .project.manage_notebooks import load_notebook
+
+# from .project.manage_notebooks import cleanup_cells
+# from .project.manage_notebooks import save_cell
+# from .project.manage_notebooks import run_cell
+# from .project.manage_notebooks import list_cell_outputs
+# from .project.manage_notebooks import list_cells
+
+# from .project.manage_concepts import list_concepts
+# from .project.manage_concepts import overview_concepts
+
+# from .project.manage_kgs import cleanup_kgs
+# from .project.manage_kgs import save_kg
+# from .project.manage_kgs import build_kg
+# from .project.manage_kgs import load_kg
+# # from .project.manage_kgs import list_kgs
+# from .project.manage_kgs import visualize_kg_concepts
+# from .project.manage_kgs import cleanup_kg_cells
+# from .project.manage_kgs import save_kg_cell
+# from .project.manage_kgs import run_kg_cell
+# from .project.manage_kgs import list_kg_cell_outputs
+# from .project.manage_kgs import list_kg_cells
+# from .project.manage_kgs import cleanup_kg_chat
+# from .project.manage_kgs import list_kg_chat
+# from .project.manage_kgs import run_kg_chat
+
+
+
+#from .project.manage_kgs import overview_kg_concepts
+
+# from .project.compile import compile
+# from .project.reason import reason
+# from .project.query import query
+# from .project.explain import explain
+# from .visualization.kg_schema_visualization import visualize_kg_schema
+# from .llm.validate import validate
+# from .llm.rag import rag
+# from .llm.translate import translate
+# from .llm.chat import chat
+# from .translation.translate_from_rdf import translate_from_rdf
+# from .project.analytics import infer_schema
+# from .project.analytics import all_pairs_join
--- prometheux_chain/project/manage_concepts.py
+++ prometheux_chain/project/manage_concepts.py
@@ -23,3 +23,21 @@ def list_concepts(project_id, project_scope="user"):
         return response.get('data', None)
     else:
         raise Exception(f"An exception occurred during listing concepts: {response.get('message', 'Unknown error')}") 
+    
+
+def overview_concepts(project_id, project_scope):
+    """
+    Overview the concepts of a specific project.
+    """
+    response = JarvisPyClient.overview_concepts(project_id, project_scope)
+
+    print(response)
+
+    if response.get('status') != 'success':
+        msg = response.get('message', 'Unknown error')
+        raise Exception(f"An exception occurred during overviewing concepts: {msg}")
+    
+    if response.get('status') == 'success':
+        return response.get('data', None)
+    else:
+        raise Exception(f"An exception occurred during overviewing concepts: {response.get('message', 'Unknown error')}")
