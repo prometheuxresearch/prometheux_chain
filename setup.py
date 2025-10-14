34bf4993a6960953d360890e266cd65423be3faf
Teodoro Baldazzi <teodoro.baldazzi@gmail.com>
2025-10-14 13:47:44 +0200

    GraphRAG added threshold and remaining configs + aligned to python >=3.9

--- setup.py
+++ setup.py
@@ -32,13 +32,15 @@ setup(
     url='https://github.com/prometheuxresearch/prometheux_chain',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.9',
 )
